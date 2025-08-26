50532fd6c5e1b3816202126b5a29bf861f66cae9
v.fateev <v.fateev@nexters.com>
2025-08-26 22:03:19 +0300

    feat(structs): enhance Person struct with contact details
    Add `ContactInfo` struct to encapsulate email and phone details. Update the `Person` struct to include `ContactInfo` as a field and initialize it in the `main` function. This enhances the example by demonstrating nested structs and their usage in Go.

--- structs/main.go
+++ structs/main.go
@@ -1,21 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Person struct {
 	firstName string
 	lastName  string
 	age       int
+	ContactInfo
+}
+
+type ContactInfo struct {
+	email string
+	phone string
 }
 
 func main() {
-	var person Person
+	person := Person{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       30,
+		ContactInfo: ContactInfo{
+			email: "some@email.com",
+			phone: "123-456-7890",
+		},
+	}
 
-	//person := Person{firstName: "John", lastName: "Doe", age: 30}
-	//
-	person.firstName = "Jane"
-	person.age = 32
+	//fmt.Println(person)
+	//fmt.Printf("%#v", person)
+	person.setAge(31)
+	person.print()
+}
 
-	fmt.Println(person)
-	fmt.Printf("%#v", person)
+func (p Person) fullName() string {
+	return p.firstName + " " + p.lastName
+}
+
+func (p Person) setAge(age int) {
+	p.age = age
+}
+
+func (p Person) print() {
+	fmt.Println(p.fullName() + " is " + strconv.Itoa(p.age) + " years old.")
 }
