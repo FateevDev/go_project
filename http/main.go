5c5d50679f444ecaed9146766364cdf5a74dbe50
vasiliy <dev.fateev@gmail.com>
2025-09-12 16:00:28 +0500

    Add LogWriter implementation to handle HTTP response streaming in main package

--- http/main.go
+++ http/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type LogWriter struct {
+}
 
 func main() {
 	resp, err := http.Get("https://google.com")
@@ -14,14 +18,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
+	defer resp.Body.Close()
 
-	_, err = resp.Body.Read(bs)
+	//bs := make([]byte, 99999)
+	//
+	//_, err = resp.Body.Read(bs)
+	//
+	//if err != nil {
+	//	fmt.Println("Error:", err)
+	//	os.Exit(1)
+	//}
+	//
+	//fmt.Println(string(bs))
 
+	//io.Copy(os.Stdout, resp.Body)
+
+	lw := LogWriter{}
+
+	io.Copy(lw, resp.Body)
+
+	os.Exit(0)
+
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
+	fmt.Println(string(body))
+}
+
+func (LogWriter) Write(bs []byte) (int, error) {
+	bl := len(bs)
+
+	fmt.Println("Write bytes: ", bl)
 	fmt.Println(string(bs))
+
+	return bl, nil
 }
